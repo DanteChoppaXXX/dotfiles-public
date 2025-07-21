2e342c4d6d0e578b7eb5ac0764d10c5d13c4422f
Dante Choppa <110491850+DanteChoppaXXX@users.noreply.github.com>
2025-07-20 23:16:47 -0400

    Create packages.sh
    List of system packages

---
+++ .scripts/packages.sh
@@ -0,0 +1,7 @@
+sudo pacman -S base-devel gcc clang lld gdb valgrind make cmake pkgconf musl \
+binutils strace ltrace radare2 capstone libseccomp libcap \
+nmap tcpdump wireshark-cli netcat inetutils iproute2 whois dnsutils \
+python python-pip nasm yasm perl \
+neovim tree-sitter clangd ccls ctags \
+zsh tmux bat eza fd ripgrep htop btop neofetch \
+iwd wget curl openssh firejail
