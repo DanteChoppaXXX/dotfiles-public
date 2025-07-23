d1637ba1bcc828c255d6ec322d67c7b8d2873247
Dante Choppa <110491850+DanteChoppaXXX@users.noreply.github.com>
2025-07-23 13:21:54 +0000

    Update ohmyposh.sh

--- .scripts/ohmyposh.sh
+++ .scripts/ohmyposh.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
-# Oh-my-posh Installation for linux
+echo "[!] Oh-My-Posh Installation for linux [!]"
+echo
+sleep 1
 # Download the correct binary for Linux (replace the URL with the latest version if needed)
-wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O oh-my-posh
+echo "[+] Downloading the correct binary for Linux... [+]"
+wget https://github.com/JanDeDobbeleer/Oh-my-posh/releases/latest/download/posh-linux-amd64 -O oh-my-posh
 
+sleep 1
+echo
 # Make the binary executable
+echo "[+] Making the binary executable [+]"
 chmod +x oh-my-posh
 
 # Move the binary to a directory in your PATH
+echo
+echo "[+] Moving the binary to /usr/local/bin/... [+]"
 sudo mv oh-my-posh /usr/local/bin/
 
 # Check Oh My Posh version to verify installation
+echo
+echo "[!] Version [!]"
 oh-my-posh --version
 
-# Configure Oh-my-posh for fish
-mkdir -p ~/.poshthemes
+echo
+echo "[!] Finally configure Oh-my-posh for your shell, then reload your shell config [!]"
+
+# Configuration example for fish shell.
+#mkdir -p ~/.poshthemes
 
  # Download the desired prompt theme
 # curl -Lo ~/.poshthemes/alien.omp.json https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/alien.omp.json
 
 # Edit fish config file "~/.config/fish/config.fish"
-oh-my-posh init fish --config ~/.poshthemes/alien.omp.json | source
+#oh-my-posh init fish --config ~/.poshthemes/alien.omp.json | source
 
 # Reload config after making any changes
-exec fish
+#exec fish
